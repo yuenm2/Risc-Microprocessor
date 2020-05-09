f0bb7784e6d707a5a72849d6f3a664fd07f7ffe7
Sheershak Agarwal <sheershaka@gmail.com>
2020-05-09 14:02:51 -0700

    Update alu.v

--- alu.v
+++ alu.v
@@ -9,28 +9,27 @@ module alu (
 	// ------------------------------------------------------------ 
 	// Outputs
 	// ------------------------------------------------------------
-	output wire [15:0] OutputAdd,
-	output wire [15:0] OutputSub,
+	output wire [16:0] OutputAdd,
+	output wire [16:0] OutputSub,
 	output wire [15:0] OutputAnd,
 	output wire [15:0] OutputOr,
 	output wire [15:0] OutputXor,
 	output wire [15:0] OutputNot,
-	output wire [15:0] OutputCMP
-	
-	output wire carry;
+	output wire [16:0] OutputCMP
+
 	// ------------------------------------------------------------ 
 	);
 
 // -------------------------------------------------------------------- 
 // Logic Declaration
 // --------------------------------------------------------------------	
-	assign {carry, OutputAdd} = op1+op2;
-	assign {carry, OutputSub} = op1+~op2+1;
+	assign OutputAdd = op1+op2;
+	assign OutputSub = op1+~op2+1;
 	assign OutputAnd = op1&op2;
 	assign OutputOr  = op1|op2;
 	assign OutputXor = op1^op2;
 	assign OutputNot = ~op1   ;
-	assign {carry, OutputCMP} = op1+~op2+1;
+	assign OutputCMP = op1+~op2+1;
 	
 // --------------------------------------------------------------------
 
